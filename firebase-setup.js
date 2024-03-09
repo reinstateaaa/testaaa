5b624ae783344109a8c225ea7fa56546ece7669c
reinstateaaausc <150784281+reinstateaaa@users.noreply.github.com>
2024-03-09 14:09:11 -0800

    Update firebase-setup.js

--- firebase-setup.js
+++ firebase-setup.js
@@ -7,13 +7,12 @@ import { getDatabase } from "firebase/database";
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
     apiKey: "AIzaSyAMh3rbnYEZkMpg-6iOVSdp6yQPH50dspg",
-  authDomain: "aaacounter.firebaseapp.com",
-  databaseURL: "https://aaacounter-default-rtdb.firebaseio.com",
-  projectId: "aaacounter",
-  storageBucket: "aaacounter.appspot.com",
-  messagingSenderId: "902544041405",
-  appId: "1:902544041405:web:e1a8e3a60fcb531abc9ed4",
-  measurementId: "G-G8K2J6WF7P",
+    authDomain: "aaacounter.firebaseapp.com",
+    projectId: "aaacounter",
+    storageBucket: "aaacounter.appspot.com",
+    messagingSenderId: "902544041405",
+    appId: "1:902544041405:web:e1a8e3a60fcb531abc9ed4",
+    measurementId: "G-G8K2J6WF7P",
     databaseURL: "https://aaacounter-default-rtdb.firebaseio.com/"
 };
 
@@ -22,4 +21,8 @@ const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 const database = getDatabase(app);
 
-export { database };
+function initializeFirebase() {
+    // Add any Firebase initialization logic here if needed
+}
+
+export { database, initializeFirebase };
