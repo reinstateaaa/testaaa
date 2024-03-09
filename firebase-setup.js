7d7af5bfd1b130b0d82e71c6e430ebe7f8ef9b98
jamescsimon <150784281+jamescsimon@users.noreply.github.com>
2024-03-09 05:36:46 -0800

    Update firebase-setup.js

--- firebase-setup.js
+++ firebase-setup.js
@@ -1,9 +1,9 @@
-<script type="module">
-  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
-  import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
-  import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
+// firebase-setup.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
 
-  const firebaseConfig = {
+const firebaseConfig = {
     apiKey: "AIzaSyAMh3rbnYEZkMpg-6iOVSdp6yQPH50dspg",
     authDomain: "aaacounter.firebaseapp.com",
     projectId: "aaacounter",
@@ -11,12 +11,8 @@
     messagingSenderId: "902544041405",
     appId: "1:902544041405:web:e1a8e3a60fcb531abc9ed4",
     measurementId: "G-G8K2J6WF7P"
-  };
+};
 
-  const app = initializeApp(firebaseConfig);
-  const analytics = getAnalytics(app);
-  const database = getDatabase(app);
-
-  const counterRef = database.ref('clickCounter');
-  counterRef.transaction((currentCount) => currentCount || 0);
-</script>
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const database = getDatabase(app);
