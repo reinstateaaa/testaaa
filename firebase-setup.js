153f4c99489a7dd8c0b84e8b93fb78871a63ba98
jamescsimon <150784281+jamescsimon@users.noreply.github.com>
2024-03-09 05:29:17 -0800

    Update firebase-setup.js

--- firebase-setup.js
+++ firebase-setup.js
@@ -1,5 +1,4 @@
 <script type="module">
-  // Import the functions you need from the SDKs you need
   import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
   import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
   import { getDatabase } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-database.js";
@@ -14,12 +13,10 @@
     measurementId: "G-G8K2J6WF7P"
   };
 
-  // Initialize Firebase
   const app = initializeApp(firebaseConfig);
   const analytics = getAnalytics(app);
   const database = getDatabase(app);
 
-  // Initialize the counter in the database if it doesn't exist
   const counterRef = database.ref('clickCounter');
   counterRef.transaction((currentCount) => currentCount || 0);
 </script>
